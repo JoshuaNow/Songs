01873f6a6476f436c5ef820f4acadb8592a57885
Joshua Lopez <jrenelopez92@gmail.com>
2021-10-14 20:52:06 -0500

    set up actions and reducers

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { selectSong } from "../actions";
 
 const App = () => {
   return <div>App</div>;
 };
 export default App;
+// I have Redux already downloaded via NPM
---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+// 2 reducers
+import { combineReducers } from "redux";
+
+export const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "Macarena", duration: "2:30" },
+    { title: "All Star", duration: "3:15" },
+    { title: "I want it That Way", duration: "1:45" },
+  ];
+};
+
+export const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
